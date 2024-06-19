e15494a2bf34c0f741eb43c95678a18d1279d3fa
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-06-19 17:55:17 +0300

    Update
    New styling and scripting for this website.
    And some links too.

---
+++ script.js
@@ -0,0 +1,10 @@
+/// Links
+// Odysee
+function OdyseeLink() {
+    window.open("https://odysee.com/@ramanauskiene.edita:4");
+}
+// Discord
+function DiscordLink() {
+    window.open("https://discord.gg/nbD4gPN2RA")
+}
+///
