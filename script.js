97f32ccd94164f59466aebcd4dbc7d1758f675f8
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-06-22 16:57:50 +0300

    Update
    New links and texts, and maybe an background image.

--- script.js
+++ script.js
@@ -3,8 +3,16 @@
 function OdyseeLink() {
     window.open("https://odysee.com/@ramanauskiene.edita:4");
 }
+// Vidlii
+function VidLiiLink() {
+    window.open("https://vidlii.com/user/ramanauskieneedita");
+}
 // Discord
 function DiscordLink() {
     window.open("https://discord.gg/nbD4gPN2RA")
+}
+// SLSG
+function SLSGLink() {
+    window.open("https://www.roblox.com/games/11629837251/Simple-LUA-Showcase-Game");
 }
 ///
