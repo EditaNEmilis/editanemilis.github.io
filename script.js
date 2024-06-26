5a5b2ee88aa0497dadcfc1ddc80d3032bf2f8b7b
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-06-26 22:54:29 +0300

    Update
    New folders for the assets.. And an .txt too! Which is Furaffinity Finest Death Threats, can be an archive anyway back from my lil trollish day.. Wah.

--- script.js
+++ script.js
@@ -15,4 +15,14 @@ function DiscordLink() {
 function SLSGLink() {
     window.open("https://www.roblox.com/games/11629837251/Simple-LUA-Showcase-Game");
 }
+/// TXTs
+// Furaffinity Finest Death Threats
+function FFDT(url, name) {
+    var link = document.createElement("a");
+    link.download = name;
+
+    url = "assets/txts/FuraffinityFinestDeathThreatsCalm.txt"
+    link.href = url;
+    link.click();
+}
 ///
