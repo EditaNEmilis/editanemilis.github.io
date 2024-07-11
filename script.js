521759933ee97767c35613109f3c72a452d7ac29
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-07-11 18:55:29 +0300

    Update
    More div, console.log, etc..

--- script.js
+++ script.js
@@ -2,23 +2,28 @@
 // Odysee
 function OdyseeLink() {
     window.open("https://odysee.com/@ramanauskiene.edita:4");
+    console.log("..Odysee..")
 }
 // Vidlii
 function VidLiiLink() {
     window.open("https://vidlii.com/user/ramanauskieneedita");
+    console.log("..VidLii..")
 }
 // Discord
 function DiscordLink() {
     window.open("https://discord.gg/nbD4gPN2RA")
+    console.log("..Discord..")
 }
 /// Games
 // SLSG
 function SLSG() {
     window.open("https://www.roblox.com/games/11629837251/Simple-LUA-Showcase-Game");
+    console.log("..Simple LUA Showcase Game..")
 }
 // ONaTI
 function ONaTI() {
     window.open("https://editagaming.itch.io/onati");
+    console.log("..One NIGHT at Treasure Island, half-baked..")
 }
 /// TXTs
 // Furaffinity Finest Death Threats
@@ -29,5 +34,6 @@ function FFDT(url, name) {
     url = "assets/txts/FuraffinityFinestDeathThreatsCalm.txt"
     link.href = url;
     link.click();
+    console.log("..Furaffinity Finest Death Threats, trollish..")
 }
 ///
