c3a529bcca3fae753c6fa777cbbad38eb92d0ae6
EditaNEmilis <ramanauskiene.edita@gmail.com>
2024-08-04 20:59:27 +0300

    Update
    New style for hovering buttons.. And also click audio! Yup, you can hear em..

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
+// Variables
+var click_audio = new Audio('assets/audio/click.wav');
+
 /// Links
 // Odysee
 function OdyseeLink() {
     window.open("https://odysee.com/@ramanauskiene.edita:4");
     console.log("..Odysee..")
+
+    click_audio.play();
 }
 // Vidlii
 function VidLiiLink() {
     window.open("https://vidlii.com/user/ramanauskieneedita");
     console.log("..VidLii..")
+
+    click_audio.play();
 }
 // Rumble
 function RumbleLink() {
     window.open("https://rumble.com/c/c-5907867");
     console.log("..Rumble..")
+
+    click_audio.play();
 }
 // Discord
 function DiscordLink() {
     window.open("https://discord.gg/nbD4gPN2RA")
     console.log("..Discord..")
+
+    click_audio.play();
 }
 /// Games
 // SLSG
 function SLSG() {
     window.open("https://www.roblox.com/games/11629837251/Simple-LUA-Showcase-Game");
     console.log("..Simple LUA Showcase Game..")
+
+    click_audio.play();
 }
 // ONaTI
 function ONaTI() {
     window.open("https://editagaming.itch.io/onati");
     console.log("..One NIGHT at Treasure Island, half-baked..")
+
+    click_audio.play();
 }
 /// TXTs
 // Furaffinity Finest Death Threats
@@ -40,5 +55,7 @@ function FFDT(url, name) {
     link.href = url;
     link.click();
     console.log("..Furaffinity Finest Death Threats, trollish..")
+
+    click_audio.play();
 }
 ///
