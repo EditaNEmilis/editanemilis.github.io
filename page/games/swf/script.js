6332140428af6b85723b6403dddf6048b465293e
EditaNEmilis <ramanauskiene.edita@gmail.com>
2025-03-31 16:50:39 +0300

    Update
    Changed up the SWF game page. Play it with Ruffle, itch.io or just download the MFA.

--- page/games/swf/script.js
+++ page/games/swf/script.js
@@ -5,7 +5,35 @@ var click_audio = new Audio('https://editanemilis.github.io/assets/audio/click.w
 // Return
 function Return() {
     window.open("https://editanemilis.github.io/page/index.html");
-    console.log("Going back..")
+    console.log("Going back..");
+
+    click_audio.play();
+}
+
+/// Buttons
+// Play SWF
+function SWFGame() {
+    window.open("https://editanemilis.github.io/assets/swf/QuickMouseSWF.swf");
+    console.log("Playing the SWF..");
+
+    click_audio.play();
+}
+// itch.io ALT
+function itchio() {
+    window.open("https://editagaming.itch.io/quick-mouse-test");
+    console.log("Moving to itch.io..");
+
+    click_audio.play();
+}
+// MFA Download
+function MFA(url, name) {
+    var link = document.createElement("a");
+    link.download = name;
+
+    url = ("https://editanemilis.github.io/assets/swf/QuickMouseSWF.mfa");
+    link.href = url;
+    link.click();
+    console.log("Downloading MFA..");
 
     click_audio.play();
 }
